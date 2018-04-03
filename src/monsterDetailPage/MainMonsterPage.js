0acf2a931bf5a43dae6cd4d040a185ee4353983f
Zach Kusz <z.s.kusz@gmail.com>
2018-04-03 18:18:41 -0500

    adds http call

--- src/monsterDetailPage/MainMonsterPage.js
+++ src/monsterDetailPage/MainMonsterPage.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
 
+import { Monster } from '../monsterList';
+
+// hhtp://.../monster/:_id
+// props _id from route params
 class MainMonsterPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            monster: {
+                _id: '',
+                name: '',
+                icon: '',
+                poison: 0,
+                sleep: 0,
+                paralysis: 0,
+                blast: 0,
+                stun: 0,
+                fire: 0,
+                water: 0,
+                thunder: 0,
+                ice: 0,
+                dragon: 0,
+                lowRankParts: [],
+                highRankParts:[],
+                searchableParts: '',
+                breakables: []
+            }
+        }; 
+    }
+
+    componentDidMount() {
+        const routeID = this.props.match.params._id;
+        if (routeID) {
+            axios.get('http://localhost:5000/monsters/' + routeID)
+            .then(res => {
+                this.setState({
+                    monster: res.data[0]
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
     }
 
     render() {
         return (
-            <h1>hi its the single monster page!</h1>
+            <div>
+                <h1>hi its the single monster page!</h1>
+                <Monster monster={this.state.monster} />
+            </div>
         );
     }
 }
